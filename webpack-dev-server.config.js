2f764d00011f97d56921a6e9ffb75cc8ee5aa2ac
wjc133 <wjc133@sina.com>
2016-11-03 21:53:33 +0800

    Initial commit

---
+++ webpack-dev-server.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const distPath = path.resolve(__dirname, 'dist');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const TransferWebpackPlugin = require('transfer-webpack-plugin');
+
+const config = {
+  // Entry points to the project
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/src/App.js'),
+  ],
+  // Server Configuration options
+  devServer: {
+    contentBase: 'src/static', // Relative directory for base of server
+    devtool: 'eval',
+    hot: true, // Live-reload
+    inline: true,
+    port: 3000, // Port Number
+    host: 'localhost', // Change to '0.0.0.0' for external facing server
+  },
+  devtool: 'eval',
+  output: {
+    path: distPath, // Path of output file
+    filename: 'App.js',
+  },
+  plugins: [
+    // Enables Hot Modules Replacement
+    new webpack.HotModuleReplacementPlugin(),
+    // Allows error warnings but does not stop compiling.
+    new webpack.NoErrorsPlugin(),
+    // Moves files
+    new TransferWebpackPlugin([
+      {from: 'static'},
+    ], path.resolve(__dirname, 'src')),
+  ],
+  module: {
+    loaders: [
+      {
+        // React-hot loader and
+        test: /\.js$/, // All .js files
+        loaders: ['react-hot', 'babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
+        exclude: [nodeModulesPath],
+      },
+    ],
+  },
+};
+
+module.exports = config;
